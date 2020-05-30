8571b4d1fe398f05043d18bba7ab804c302d06de
pgzulfaqar <jamjam.zubair@gmail.com>
2020-05-30 12:05:53 +0800

    Before cropping

---
+++ vid_detector_min.py
@@ -0,0 +1,112 @@
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from threading import Thread
+from imutils.video import WebcamVideoStream
+#from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+
+def detect_and_predict_mask(frame, faceNet):
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
+		(104.0, 177.0, 123.0))
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	faces = []
+	locs = []
+
+	# loop over the detections
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+		if confidence > 0.5:
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+			face = np.expand_dims(face, axis=0)
+			# add the face and bounding boxes to their respective
+			# lists
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+
+	return (locs)
+
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self,resolution=(640,480),framerate=30):
+        # Initialize the PiCamera and the camera image stream
+        self.stream = cv2.VideoCapture(0)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3,resolution[0])
+        ret = self.stream.set(4,resolution[1])
+            
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+	# Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+	# Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        while True:
+
+            if self.stopped:
+                self.stream.release()
+                return
+
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        return self.frame
+
+    def stop(self):
+        self.stopped = True
+
+prototxtPath = "face_detector/deploy.prototxt"
+weightsPath = "face_detector/res10_300x300_ssd_iter_140000.caffemodel"
+
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+vs = VideoStream(resolution=(400,320),framerate=30).start()
+time.sleep(1)
+
+while (1):
+	frame = vs.read()
+	frame = cv2.resize(frame, (400,320))
+	#frame = np.expand_dims(frame_resized, axis=0)
+	#frame = imutils.resize(frame, width=400)
+
+	#ret, huge_frame = vs.read()
+	#frame = cv2.resize(huge_frame, (480,320))
+
+	(locs) = detect_and_predict_mask(frame, faceNet)
+
+	for (box) in locs:
+		(startX, startY, endX, endY) = box
+		color = (0, 0, 255)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+		break
+
+cv2.destroyAllWindows()
+vs.stop()
